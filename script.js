3690b8d95f25a5a873e3ace562fc8c8ed696a330
SameerAhmed419 <145706331+SameerAhmed419@users.noreply.github.com>
2023-09-24 16:35:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-   // Form Handler
+"use strict";
+    let w = innerWidth;
+    let h = innerHeight;
+   // Well-Responsive Layout With An Event Listener
+   document.addEventListener("DOMContentLoaded", sizechange );
+   // The Function sizechange is global
+   function sizechange() {
+       document.getElementById("zero").style.height = (0.03 * h) + "px";
+document.getElementById("zero").style.maxHeight = (0.03 * h) + "px"; 
+document.getElementById("zero").style.marginBottom = (0.02 * h) + "px" ;  
+document.getElementById("main").style.height = (0.46 * h) + "px";
+document.getElementById("main").style.marginBottom = (0.01 * h) + "px";  
+document.getElementById("main").style.maxHeight = (0.46 * h) + "px"; 
+document.getElementById("third").style.height = (0.44 * h) + "px";
+document.getElementById("third").style.marginBottom = (0.01 * h) + "px";
+document.getElementById("fourth").style.height = (0.03 * h) + "px";
+document.getElementById("fourth").style.maxHeight = (0.03 * h) + "px";
+console.log(h + "and" + w);
+} 
+   // The Layout Handler Ends Here  
+   // Form Handler for the Button
 document.querySelector("#form").addEventListener("submit", guide);
+// The Function guide is the Boss
 function guide(event) {
     console.log(event);
     event.preventDefault();
-    // Both the Divisions Must Give Space to the Frame
-      let tohide = document.getElementsByClassName("hid");
-      for (let n = 0; n < tohide.length; n++) {
-      tohide[n].style.display = "none";
-      }
-   // A Button to make it reappear
-         let btn =  document.createElement("button");
-     document.querySelector("#main").appendChild(btn);
-     btn.innerHTML = "Uncover";
-     btn.id = "restore";
-     btn.style = "border: none; font-size: vw; width: vw; height: 5%; margin: 0 auto 3%; color: #0f5742; background-color: white; font-weight: bold";
-   // The Script for the Button
-   document.getElementById("restore").addEventListener("click", restore);
-   function restore() {
-   // Unhide the Divisions
-       let toshow = document.getElementsByClassName("hid");
-      for (let i = 0; i < toshow.length; i++) {
-      toshow[i].style.display = "flex";
-}
-   // Remove the Restore Button
-      document.getElementById("restore").remove();
-      }
+   // An Event Handler to make them reappear
+     document.getElementById("hide1").addEventListener("click", hideseek);
+     document.getElementById("hide2").addEventListener("click", hideseek);
+     // The Function Hideseek is limited to the Function guide
+     function hideseek() {
+         let anivalue = window.getComputedStyle(this.parentNode).getPropertyValue("animation-play-state");
+         console.log(anivalue);
+     }
    // val Will hold the value of the selected option
    let checkvalue = document.querySelector("input[name='eng']:checked");
    let val;
+   // Checks Whether an Option is Selected
    if (checkvalue != null) {
        val = document.querySelector("input[name='eng']:checked").value; 
+    // The Button Closes Both the Menus
+    let hideshow = document.getElementsByClassName("hid");
+     for (let n=0; n < hideshow.length; n++) {
+         hideshow[n].style.animationPlayState = "running";
+     } 
    }
    else {
        val = 0;
@@ -62,8 +75,14 @@ function guide(event) {
          console.log("case 5");
          url = "https://www.youtube.com/embed/at1s-OqWb_A";
         break;
+        case "f":
+        // His Financial Situation
+         console.log();
+         url = "https://www.youtube.com/embed/PHq_uue0OQg";
+        break;
         case 0:
-         restore();
+         sizechange();
+         
          return;
         break;
     }
@@ -79,9 +98,11 @@ function guide(event) {
     contain.appendChild(frame);
    // The Frame is to be Adjusted
     frame.src = url;
-    frame.width = "90%";
-    frame.height = "90%";
-    frame.style.border = "none";
-   // The Container is to be Revealed
-    contain.style.display = "flex";
+    frame.id = "vid";
+    frame.width = w * 0.8;
+    frame.height = h / 3;
+    frame.style.margin = (0.055 * h) + "px";    
+   // The Container is to be Adjusted
+    contain.style.visibility = "visible";
 }
+// The Button's Event Handler Ends Here
